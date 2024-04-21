6482ab041065501105c7f2c130d8dac3aea04e97
santibaq <s.baquerov@uniandes.edu.co>
2024-04-20 19:29:14 -0500

    Clase Pieza

---
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Fotografia.java
@@ -0,0 +1,25 @@
+package dpoo.galeria.modelo.pieza;
+
+public class Fotografia extends Pieza {
+
+	public String resolucionFoto;
+	public String plano;
+	public String relacionAspecto;
+	public String formato;
+	public String generoFotografico;
+	public String tecnica;
+	
+	public Fotografia () {
+		
+		this.resolucionFoto = resolucionFoto;
+		this.plano = plano;
+		this.relacionAspecto = relacionAspecto;
+		this.formato = formato;
+		this.generoFotografico = generoFotografico;
+		this.tecnica = tecnica;
+	}
+	
+	/*
+	 * Métodos de Fotografia (Si los tiene).
+	 */
+}
---
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/trabajador/Trabajador.java
@@ -0,0 +1,21 @@
+package dpoo.galeria.modelo.trabajador;
+
+public abstract class Trabajador {
+
+	private String nombre;
+	private int celular;
+	private String ID;
+	private String password;
+	
+	public Trabajador () {
+		
+		this.nombre = nombre;
+		this.celular = celular;
+		this.ID = ID;
+		this.password = password;
+	}
+	
+	/*
+	 * Metodos de Trabajador (Si los tiene).
+	 */
+}
