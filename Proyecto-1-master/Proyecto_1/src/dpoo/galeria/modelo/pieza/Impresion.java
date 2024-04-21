755888d866472df90a95cb517ad45b1612f0bd2f
santibaq <s.baquerov@uniandes.edu.co>
2024-04-20 19:46:01 -0500

    Clase impresion

--- Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Impresion.java
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Impresion.java
@@ -1,21 +1,56 @@
 package dpoo.galeria.modelo.pieza;
 
-public class Impresion extends Pieza {
+import java.util.Date;
+import java.util.List;
 
-	public double alto;
-	public double ancho;
-	public String tipoPapel;
-	public String formatoImpresion;
-	
-	public Impresion () {
-		
-		this.alto = alto;
-		this.ancho = ancho;
-		this.tipoPapel = tipoPapel;
-		this.formatoImpresion = formatoImpresion;
-	}
-	
-	/*
-	 * Métodos de Impresion (Si los tiene).
-	 */
+
+public class Impresion extends Pieza {
+	private double alto;
+    private double ancho;
+    private String tipoPapel;
+    private String formatoImpresion;
+
+
+	public Impresion(String titulo, Date fechaCreacion, String lugarCreacion, List<String> autores,
+			String modalidadVenta, Date fechaEntrega, int ID, String IDpropietario, List<String> detallesAdicionales, double alto, double ancho,
+            String tipoPapel, String formatoImpresion) {
+		super(titulo, fechaCreacion, lugarCreacion, autores, modalidadVenta, fechaEntrega, ID, IDpropietario,
+				detallesAdicionales);
+	this.alto = alto;
+    this.ancho = ancho;
+    this.tipoPapel = tipoPapel;
+    this.formatoImpresion = formatoImpresion;
+}
+
+	public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public String getTipoPapel() {
+        return tipoPapel;
+    }
+
+    public void setTipoPapel(String tipoPapel) {
+        this.tipoPapel = tipoPapel;
+    }
+
+    public String getFormatoImpresion() {
+        return formatoImpresion;
+    }
+
+    public void setFormatoImpresion(String formatoImpresion) {
+        this.formatoImpresion = formatoImpresion;
+    }
 }
