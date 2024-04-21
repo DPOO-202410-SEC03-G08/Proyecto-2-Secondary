c2a164a54e5667a087418202cbaa0ff948d8ff52
santibaq <s.baquerov@uniandes.edu.co>
2024-04-20 19:44:38 -0500

    Clase Escultura

--- Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Escultura.java
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Escultura.java
@@ -1,27 +1,73 @@
 package dpoo.galeria.modelo.pieza;
-
+import java.util.Date;
 import java.util.List;
 
-public class Escultura extends Pieza{
+public class Escultura extends Pieza {
+	private double alto;
+    private double ancho;
+    private double profundidad;
+    private List<String> materialesConstruccion;
+    private double peso;
+    private boolean necesitaElectricidad;
 
-	public double alto;
-	public double ancho;
-	public double profundidad;
-	public List<String> materialConstruccion;
-	public double peso;
-	public boolean necesitaElectricidad;
-	
-	public Escultura () {
-		
+	public Escultura(String titulo, Date fechaCreacion, String lugarCreacion, List<String> autores,
+			String modalidadVenta, Date fechaEntrega, int ID, String IDpropietario, List<String> detallesAdicionales, double alto, double ancho, double profundidad,
+            List<String> materialesConstruccion, double peso, boolean necesitaElectricidad) {
+		super(titulo, fechaCreacion, lugarCreacion, autores, modalidadVenta, fechaEntrega, ID, IDpropietario,
+				detallesAdicionales);
 		this.alto = alto;
-		this.ancho = ancho;
-		this.profundidad = profundidad;
-		this.materialConstruccion = materialConstruccion;
-		this.peso = peso;
-		this.necesitaElectricidad = necesitaElectricidad;
-	}
-	
-	/*
-	 * Métodos de Escultura (Si los tiene).
-	 */
+        this.ancho = ancho;
+        this.profundidad = profundidad;
+        this.materialesConstruccion = materialesConstruccion;
+        this.peso = peso;
+        this.necesitaElectricidad = necesitaElectricidad;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getProfundidad() {
+        return profundidad;
+    }
+
+    public void setProfundidad(double profundidad) {
+        this.profundidad = profundidad;
+    }
+
+    public List<String> getMaterialesConstruccion() {
+        return materialesConstruccion;
+    }
+
+    public void setMaterialesConstruccion(List<String> materialesConstruccion) {
+        this.materialesConstruccion = materialesConstruccion;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public boolean getNecesitaElectricidad() {
+        return necesitaElectricidad;
+    }
+
+    public void setNecesitaElectricidad(boolean necesitaElectricidad) {
+        this.necesitaElectricidad = necesitaElectricidad;
+    }
 }
