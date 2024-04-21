04e7f6f5b2212fa3699b1cc34d20c85897f7a576
santibaq <s.baquerov@uniandes.edu.co>
2024-04-20 19:47:15 -0500

    Clase video

--- Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Video.java
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Video.java
@@ -1,23 +1,67 @@
 package dpoo.galeria.modelo.pieza;
 
-public class Video extends Pieza {
+import java.util.Date;
 
-	public int duracion;
-	public String resolucionVideo;
-	public int FPS;
-	public String relacionAspecto;
-	public String formato;
-	
-	public Video () {
-		
+import java.util.List;
+
+
+public class Video extends Pieza {
+    private int duracion;
+    private String resolucionVideo;
+    private int fotogramasPorSegundo;
+    private String relacionAspecto;
+    private String formato;
+
+	public Video(String titulo, Date fechaCreacion, String lugarCreacion, List<String> autores, String modalidadVenta,
+			Date fechaEntrega, int ID, String IDpropietario, List<String> detallesAdicionales, int duracion, String resolucionVideo,
+            int fotogramasPorSegundo, String relacionAspecto, String formato) {
+		super(titulo, fechaCreacion, lugarCreacion, autores, modalidadVenta, fechaEntrega, ID, IDpropietario,
+				detallesAdicionales);
 		this.duracion = duracion;
-		this.resolucionVideo = resolucionVideo;
-		this.FPS = FPS;
-		this.relacionAspecto = relacionAspecto;
-		this.formato = formato;
+        this.resolucionVideo = resolucionVideo;
+        this.fotogramasPorSegundo = fotogramasPorSegundo;
+        this.relacionAspecto = relacionAspecto;
+        this.formato = formato;
 	}
-	
-	/*
-	 * Métodos de Video (si los tiene)
-	 */
+
+	public int getDuracion() {
+        return duracion;
+    }
+
+    public String getResolucionVideo() {
+        return resolucionVideo;
+    }
+
+    public int getFotogramasPorSegundo() {
+        return fotogramasPorSegundo;
+    }
+
+    public String getRelacionAspecto() {
+        return relacionAspecto;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setResolucionVideo(String resolucionVideo) {
+        this.resolucionVideo = resolucionVideo;
+    }
+
+    public void setFotogramasPorSegundo(int fotogramasPorSegundo) {
+        this.fotogramasPorSegundo = fotogramasPorSegundo;
+    }
+
+    public void setRelacionAspecto(String relacionAspecto) {
+        this.relacionAspecto = relacionAspecto;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
 }
+
