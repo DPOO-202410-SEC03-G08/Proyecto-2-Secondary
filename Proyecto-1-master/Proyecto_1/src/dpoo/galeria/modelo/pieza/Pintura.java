24032b998621a54a786102452a3bea1f74f8f641
santibaq <s.baquerov@uniandes.edu.co>
2024-04-20 19:46:42 -0500

    Clase pintura

--- Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Pintura.java
+++ Proyecto-1-master/Proyecto_1/src/dpoo/galeria/modelo/pieza/Pintura.java
@@ -1,23 +1,59 @@
 package dpoo.galeria.modelo.pieza;
 
+import java.util.Date;
+
 import java.util.List;
 
 public class Pintura extends Pieza {
+    private List<String> color;
+    private String tecnica;
+    private String estilo;
+    private String superficie;
 
-	public List<String> colores;
-	public String tecnica;
-	public String estilo;
-	public String superficie;
-	
-	public Pintura () {
-		
-		this.colores = colores;
-		this.tecnica = tecnica;
-		this.estilo = estilo;
-		this.superficie = superficie;
-	}
-	
-	/*
-	 * Metodos de Pintura (Si los tiene)
-	 */
+    public Pintura(String titulo, Date fechaCreacion, String lugarCreacion, List<String> autores,
+                   String modalidadVenta, Date fechaEntrega, int ID, String IDpropietario,
+                   List<String> detallesAdicionales, List<String> color, String tecnica, 
+                   String estilo, String superficie) {
+    	super(titulo, fechaCreacion, lugarCreacion, autores, modalidadVenta, 
+              fechaEntrega, ID, IDpropietario, detallesAdicionales);
+        this.color = color;
+        this.tecnica = tecnica;
+        this.estilo = estilo;
+        this.superficie = superficie;
+    }
+
+
+    public List<String> getColor() {
+        return color;
+    }
+
+    public void setColor(List<String> color) {
+        this.color = color;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(String superficie) {
+        this.superficie = superficie;
+    }
+
+
 }
